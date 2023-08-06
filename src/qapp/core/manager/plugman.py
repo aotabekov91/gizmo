ca38db6b06ecf1742777e0bdfcb6a59150b799c4
adam <adam@myworld.net>
2023-08-06 22:12:02 +0200

    minor

--- src/qapp/core/manager/plugman.py
+++ src/qapp/core/manager/plugman.py
@@ -11,6 +11,9 @@ class Plugman:
         self.app=app
         self.path=os.path.join(self.app.path, 'plugs')
 
+        self.app.intents=[]
+        self.app.entities=[]
+
         self.plugs={}
 
     def cleanup(self): pass #TODO
@@ -23,7 +26,11 @@ class Plugman:
             sys.path.append(self.path)
             for path in os.listdir(self.path):
                 self.read(path, register=False)
+
             self.app.actionRegistered.emit()
+
+            paths=[self.app.entities, self.app.intents]
+            self.app.registerByUmay(paths)
 
     def read(self, plug_path, register=True):
 
@@ -40,6 +47,18 @@ class Plugman:
         plug=plug_class(app=self.app)
         self.plugs[plug.name]=plug
         setattr(self, plug.name, plug)
+
+        if plug.intents:
+            for i in plug.intents:
+                print(i)
+                name=i['name']
+                i['name']=f"{self.app.name}_{name}"
+            self.app.intents+=plug.intents
+        if plug.entities:
+            for i in plug.intents:
+                name=i['intentName']
+                i['intentName']=f"{self.app.name}_{name}"
+            self.app.entities+=plug.entities
 
         # except:
         #     print('Could not not plug: ', plug_class.__name__)
