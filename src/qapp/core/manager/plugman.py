bfc84985d62538241ef0a4317318431aa488808a
adam <adam@myworld.net>
2023-08-06 13:37:11 +0200

    minor

--- src/qapp/core/manager/plugman.py
+++ src/qapp/core/manager/plugman.py
@@ -1,39 +1,45 @@
 import os
 import sys
 import importlib
-from PyQt5 import QtCore
 
-class Plugman(QtCore.QObject):
+class Plugman:
 
     def __init__(self, app):
 
-        super(Plugman, self).__init__(app)
+        super(Plugman, self).__init__()
 
         self.app=app
+        self.path=os.path.join(self.app.path, 'plugs')
+
         self.plugs={}
+
+    def cleanup(self): pass #TODO
+
+    def install(self): pass #TODO
 
     def load(self):
 
-        self.plugs_path=os.path.join(self.app.path, 'plugs')
+        if os.path.exists(self.path):
+            sys.path.append(self.path)
+            for path in os.listdir(self.path):
+                self.read(path, register=False)
+            self.app.actionRegistered.emit()
 
-        if os.path.exists(self.plugs_path):
-            sys.path.append(self.plugs_path)
-            for n in os.listdir(self.plugs_path):
-                if not n.startswith('__'):
-                    pmod=importlib.import_module(n)
-                    if hasattr(pmod, 'get_plug_class'):
-                        self.addPlug(pmod.get_plug_class())
+    def read(self, plug_path, register=True):
 
-        self.app.actionRegistered.emit()
+        plug_module=importlib.import_module(plug_path)
+        if hasattr(plug_module, 'get_plug_class'):
+            self.initiate(plug_module.get_plug_class())
 
-    def addPlug(self, plug_class, action_register=False):
+        if register: self.app.actionRegistered.emit()
+
+    def initiate(self, plug_class):
 
         # try:
 
         plug=plug_class(app=self.app)
         self.plugs[plug.name]=plug
         setattr(self, plug.name, plug)
-        if action_register: self.app.actionRegistered.emit()
 
         # except:
         #     print('Could not not plug: ', plug_class.__name__)
