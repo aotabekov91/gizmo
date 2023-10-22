dc55cd9a314e57f8ba4f72f36a24158c7f6818c2
adam <adam@myworld.net>
2023-10-22 06:05:27 +0200

    minor

--- tests/conftest.py
+++
@@ -1,5 +0,0 @@
-import sys
-from pathlib import Path
-
-dir_path=Path(Path(__file__).parent.parent)
-sys.path.insert(0, str(dir_path.joinpath('src')))
--- tests/test_mode.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from qapp.core.modes import Mode
-
-@pytest.fixture
-def mode(): return Mode()
-
-def test_import(mode): assert mode
-
