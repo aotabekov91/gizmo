5304953d93ed49fa99d79dd6e461473458e713c5
adam <adam@myworld.net>
2023-11-11 19:31:26 +0100

    added dellocator to locate

---
+++ src/gizmo/vimo/model/mixin/locate.py
@@ -0,0 +1,15 @@
+class Locate:
+
+    canLocate=True
+
+    def delLocator(self, data=None, kind=None):
+        return {}
+
+    def getLocator(self, data=None, kind=None):
+        return {}
+    
+    def getUniqLocator(self, data=None, kind=None):
+        return {}
+
+    def setLocator(self, data=None, kind=None):
+        pass
