e82f3a07277b4d12de64ac7949285204317dab58
adam <adam@myworld.net>
2023-11-12 02:03:22 +0100

    updated locate mixin

--- src/gizmo/vimo/model/mixin/locate.py
+++ src/gizmo/vimo/model/mixin/locate.py
@@ -1,15 +1,39 @@
 class Locate:
 
     canLocate=True
+    
+    def findLocator(self, prefix, kind):
+
+        if kind:
+            n=f'{prefix}{kind.title()}Locator'
+            return getattr(self, n, None)
 
     def delLocator(self, data=None, kind=None):
+
+        f=self.findLocator('del', kind)
+        if f: return f(data)
         return {}
 
     def getLocator(self, data=None, kind=None):
+
+        f=self.findLocator('get', kind)
+        if f: return f(data)
         return {}
     
     def getUniqLocator(self, data=None, kind=None):
+
+        f=self.findLocator('getUniq', kind)
+        if f: return f(data)
         return {}
 
     def setLocator(self, data=None, kind=None):
-        pass
+
+        f=self.findLocator('set', kind)
+        if f: return f(data)
+        return {}
+
+    def openLocator(self, data=None, kind=None):
+
+        f=self.findLocator('open', kind)
+        if f: return f(data)
+        return {}
